9afcedf15d0551a0b6bb9211052d2b1e9cb42b36
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-03-06 00:08:50 +0100

    Now, commit on the wright branch

---
+++ raspimote_https/__init__.py
@@ -0,0 +1,15 @@
+"""Fork of Cheroot. It improves the web server, especially by adding verbose options. It is used as an HTTPS server in RaspiMote releases."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+try:
+    import pkg_resources
+except ImportError:
+    pass
+
+
+try:
+    __version__ = pkg_resources.get_distribution('cheroot').version
+except Exception:
+    __version__ = 'unknown'
